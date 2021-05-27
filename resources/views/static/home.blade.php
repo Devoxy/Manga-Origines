b3286b8bfbac551ad94e13683627b1abf77cb290
Nitram <martin.elouan.77@gmail.com>
2021-05-27 19:48:03 +0700

    top manga

--- resources/views/static/home.blade.php
+++ resources/views/static/home.blade.php
@@ -35,7 +35,18 @@
             </a>
         </nav>
         <div class="header__tools">
-            sdjfldjsfsjdfkdl
+            <div class="header__tools_search">
+                sdfssdfd
+            </div>
+            <div class="header__tools_notifications">
+                sdfssdfd
+            </div>
+            <div class="header__tools__account">
+                <img src="/images/static/login.png">
+                <div class="header__tools__account__panel">
+                    <a href="#">tesst</a>
+                </div>
+            </div>
         </div>
     </header>
     <div class="recent" id="recent">
@@ -214,30 +225,7 @@
         </div>
     </div>
 
-    <div class="top-mangas" id="top-mangas">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 col-md-6">
-                    <div class="top-mangas__image">
-                        <img src="/images/front/top-1.png">
-                    </div>
-                </div>
-                <div class="col-12 col-md-3">
-                    <div class="top-mangas__image max">
-                        <img src="/images/front/top-2.png">
-                    </div>
-                </div>
-                <div class="col-12 col-md-3 d-flex flex-column">
-                    <div class="top-mangas__image">
-                        <img src="/images/front/top-3.png">
-                    </div>
-                    <div class="top-mangas__image">
-                        <img src="/images/front/top-4.png">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('static/topmanga')
 
     <script src="/js/jquery-2.1.4.min.js"></script>
     <script type="text/javascript">
---
+++ resources/views/static/topmanga.blade.php
@@ -0,0 +1,24 @@
+<div class="top-mangas" id="top-mangas">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div class="top-mangas__image">
+                    <img src="/images/front/top-1.png">
+                </div>
+            </div>
+            <div class="col-12 col-md-3">
+                <div class="top-mangas__image max">
+                    <img src="/images/front/top-2.png">
+                </div>
+            </div>
+            <div class="col-12 col-md-3 d-flex flex-column">
+                <div class="top-mangas__image">
+                    <img src="/images/front/top-3.png">
+                </div>
+                <div class="top-mangas__image">
+                    <img src="/images/front/top-4.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
