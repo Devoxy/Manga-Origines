01869d0e73281361c5d55fdac2e3c3e6e1ac585e
Riotor Mathias <mathias.riotor@gmail.com>
2021-08-03 00:22:00 +0900

    push vip

--- resources/views/static/vip.blade.php
+++ resources/views/static/vip.blade.php
@@ -13,5 +13,49 @@
             VOUS SOUHAITEZ NOUS SOUTENIR TOUT EN AYANT ACCÈS À DU CONTENUS EXCLUSIF ?
         </h3>
     </div>
+
+    <div class="vip-bigtitle">
+        <h2>
+            DEVENEZ <span>VIP !</span><br>
+            DÉCOUVREZ NOS OFFRES !
+        </h2>
+    </div>
+
+    <div class="vip-pricing">
+        <div class="vip-pricing__table">
+            <div class="vip-pricing__table__icon">
+                <img src="/images/static/icons/bonfire.png">
+            </div>
+            <div class="vip-pricing__table__title">Soldat</div>
+            <div class="vip-pricing__table__description">
+            Tu souhaites le strict minimum ? Cette offre te conviendra parfaitement, soldat.
+            </div>
+        </div>
+
+        <div class="vip-pricing__table">
+            <div class="vip-pricing__table__icon">
+                <img src="/images/static/icons/king.png">
+            </div>
+            <div class="vip-pricing__table__title">King</div>
+            <div class="vip-pricing__table__description">
+            Tu es avides et tu désires que tout puisses t'appartenir ? Ce pack est fait pour toi, alors.
+            </div>
+        </div>
+
+        <div class="vip-pricing__table recommanded">
+            <div class="vip-pricing__table__icon">
+                <img src="/images/static/icons/diamond.png">
+            </div>
+            <div class="vip-pricing__table__title">Gardien</div>
+            <div class="vip-pricing__table__description">
+            Tu veux un accès prioritaire pour nos dernières publications ? Voilà ce qu'il te faut.
+            </div>
+        </div>
+
+    </div>
+
+    <div class="vip-banner">
+        <img src="/images/static/background-vip.png" style="width: 100%; padding: 40px 0px;">
+    </div>
 </div>
 @endsection
