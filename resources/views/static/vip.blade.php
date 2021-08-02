251500067167cc08101f2313bcdde2c519de2dc8
Riotor Mathias <mathias.riotor@gmail.com>
2021-08-02 23:55:22 +0900

    jj

--- resources/views/static/vip.blade.php
+++ resources/views/static/vip.blade.php
@@ -5,7 +5,13 @@
 @section('content')
 <div class="container">
     <div class="vip-banner">
-        <img src="./images/static/ad.png" style="width: 100%; padding: 40px 0px;">
+        <img src="/images/static/background-vip.png" style="width: 100%; padding: 40px 0px;">
+    </div>
+
+    <div class="vip-title">
+        <h3>
+            VOUS SOUHAITEZ NOUS SOUTENIR TOUT EN AYANT ACCÈS À DU CONTENUS EXCLUSIF ?
+        </h3>
     </div>
 </div>
 @endsection
