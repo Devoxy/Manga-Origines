0aa3684b6f61f2005be5e2f54d88c3ac4fdb5ab8
Nitram <martin.elouan.77@gmail.com>
2021-08-02 21:35:17 +0700

    d

--- resources/views/static/contact.blade.php
+++ resources/views/static/contact.blade.php
@@ -6,12 +6,15 @@
 <div class="container" style="margin-top: 50px;">
     <div class="container-sidebar">
         <div class="container-sidebar__content">
-            <div class="block contact" id="contact">
+            <div class="block" id="contact">
                 <div class="block__title contact">
                     <h3>Contactez-nous !</h3>
                 </div>
                 <div class="block__content">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut neque ante, commodo non aliquam vel, fermentum in ante. Nam accumsan enim eu odio posuere tincidunt. Integer imperdiet ac enim in rhoncus. Ut at facilisis quam. Quisque lacus quam, tempor quis libero ac, tincidunt placerat sapien.</p>
+                    <div class="contact">
+                        sdfsdf
+                    </div>
                 </div>
             </div>
         </div>
