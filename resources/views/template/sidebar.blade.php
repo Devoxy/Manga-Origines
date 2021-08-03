7911cfeec5ea954cd59be5c2502cd9e5214c5267
Nitram <martin.elouan.77@gmail.com>
2021-08-03 10:37:59 +0700

    fin contact

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -1,1 +1,8 @@
-dfd
+<div class="container-sidebar__sidebar__block">
+    <div class="container-sidebar__sidebar__block__title discord">
+        <h5>Notre Discord</h5>
+    </div>
+    <div class="container-sidebar__sidebar__block__content">
+        <iframe src="https://discord.com/widget?id=749473022821400596&theme=light" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+    </div>
+</div>
